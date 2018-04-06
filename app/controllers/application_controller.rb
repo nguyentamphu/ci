5683736c3af465477739784df2d6c09f5bac4f1c
PHU Nguyen Tam <phunt@runsystem.net>
2018-04-06 10:25:29 +0700

    add bad method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  class Bad
+    def im_bad_method
+      x = "lol"
+      10.times.each {|t| t.to_i}
+      return x
+    end
+  end
 end
